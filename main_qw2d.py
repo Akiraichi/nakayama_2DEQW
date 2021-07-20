9a299ffbae0b4af394d8934906b0bfcadd16832b
aki <s2111@gm.kochi-ct.jp>
2021-07-20 15:26:28 +0900

    不要なコードを消した

--- main_qw2d.py
+++ main_qw2d.py
@@ -6,76 +6,29 @@ from simulation.simulation_mamoery_save import execute_simulation_memory_save
 # plot
 from analyze.visualization.plot_memory_save import execute_plot_memory_save_2
 from analyze.visualization.plot_phase_save import execute_plot_phase
+from analyze.visualization.plot_heatmap import execute_plot_heatmap
 # gif
-from analyze.visualization.make_gif import make_gif, make_gif_phase
-
-
-# def main_loop(conditions, exp_name):
-#     while True:
-#         try:
-#             print("simuのスタート")
-#             execute_simulation_memory_save(conditions, exp_name)
-#
-#             print("plotのスタート")
-#             execute_plot_memory_save_2(exp_name)
-#
-#             print("gifのスタート")
-#             make_gif(exp_name)
-#         except Exception as e:
-#             print("Error発生")
-#             print(e)
-#             # システムをリスタートする
-#             import os
-#             import sys
-#
-#             os.execl(sys.executable, 'python3', __file__)
-#         else:
-#             print('finish (no error)')
-#             print("システム完了")
-#             break
-#
-#
-# def main_loop_phase(conditions, exp_name):
-#     while True:
-#         try:
-#             print("simuのスタート")
-#             execute_simulation_memory_save(conditions, exp_name)
-#
-#             for plot_t_step in [1, 2, 3, 4, 5, 9]:
-#                 print("plotのスタート")
-#                 execute_plot_phase(exp_name, plot_t_step)
-#                 print("gifのスタート")
-#                 make_gif_phase(exp_name, plot_t_step)
-#
-#         except Exception as e:
-#             print("Error発生")
-#             print(e)
-#             # システムをリスタートする
-#             import os
-#             import sys
-#
-#             os.execl(sys.executable, 'python3', __file__)
-#         else:
-#             print('finish (no error)')
-#             print("システム完了")
-#             break
-
+from analyze.visualization.make_gif import make_gif, make_gif_phase, make_gif_heatmap
 
 if __name__ == '__main__':
     # デバッグ1
-    conditions, exp_name = exp_5010_x_set(1,21)
-    execute_simulation_memory_save(conditions, exp_name)
-    execute_plot_phase(exp_name,100)
-    make_gif_phase(exp_name,100)
-    # main_loop(conditions, exp_name)
+    conditions, exp_name = exp_5010_x_set(1, 21)
+    # execute_simulation_memory_save(conditions, exp_name)
+    # execute_plot_memory_save_2(exp_name)
+    # execute_plot_phase(exp_name, 100)
+    execute_plot_heatmap(exp_name, t_step=100)
+    # make_gif(exp_name, 100, 20)
+    make_gif_heatmap(exp_name, 100)
 
-    # execute_plot_memory_save_2(exp_name=exp_name)
-    # main_loop_phase(conditions, exp_name)
-    # execute_plot_phase(exp_name, 9)
-    # デバッグ2
-    # conditions, exp_name = exp_0000__debug_folder_changed_check_set()
-    # main_loop(conditions, exp_name)
-
-    # デバッグ3
-    # plot_t_step = 99
-    # make_gif_phase(exp_name=exp_name, plot_t_step=plot_t_step)
+    # import joblib
+    # from simulation.algorithm_memory_save import calculate_probability_distribution_at_time_t_memory_save
+    #
+    # T = 100
+    # len_x = 201
+    # len_y = 201
+    # file_name = 'result/exp_5010/simulation_data_exp_5010/00/013.jb'
+    # PSY = joblib.load(file_name)
+    # mesh_x, mesh_y = np.meshgrid(np.linspace(-T, T, 2 * T + 1), np.linspace(-T, T, 2 * T + 1), indexing="ij")
+    # mesh_z = calculate_probability_distribution_at_time_t_memory_save(PSY, len_x, len_y)
+    #
+    # plot_heat_map_probability(mesh_z)
