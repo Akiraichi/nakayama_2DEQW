172e7f237eb45cf18f577d7ffca1028f0a7136c1
aki <s2111@gm.kochi-ct.jp>
2021-07-21 22:24:25 +0900

    加筆

--- main_qw2d.py
+++ main_qw2d.py
@@ -11,10 +11,14 @@ from analyze.visualization.make_gif import make_gif, make_gif_phase, make_gif_he
 
 if __name__ == '__main__':
     # デバッグ1
-    conditions, exp_name = exp_5010_x_set(1, 21)
+    conditions, exp_name = exp_016_01_00_x_set_debug(60,61)
     execute_simulation_memory_save(conditions, exp_name)
+
     # execute_plot_memory_save_2(exp_name)
+    # make_gif(exp_name)
+
     execute_plot_phase(exp_name, 100)
-    execute_plot_heatmap(exp_name, t_step=100)
-    make_gif_phase(exp_name, 100)
-    make_gif_heatmap(exp_name, 100)
+    # make_gif_phase(exp_name, 100)
+
+    # execute_plot_heatmap(exp_name, t_step=100)
+    # make_gif_heatmap(exp_name, 100)
