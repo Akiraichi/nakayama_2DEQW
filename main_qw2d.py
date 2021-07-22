f5e8d8d49f5942234eea952cac19f2a1ee1fb504
aki <s2111@gm.kochi-ct.jp>
2021-07-22 15:33:36 +0900

    名称の変更

--- main_qw2d.py
+++ main_qw2d.py
@@ -3,22 +3,25 @@ from exp_setting.exp import *
 # simulation
 from simulation.simulation_mamoery_save import execute_simulation_memory_save
 # plot
-from analyze.visualization.plot_memory_save import execute_plot_memory_save_2
-from analyze.visualization.plot_phase_save import execute_plot_phase
-from analyze.visualization.plot_heatmap import execute_plot_heatmap
+from analyze.visualization.plot_memory_save import execute_plot_surface
+from analyze.visualization.plot_phase_save import execute_plot_surface_by_phase
+from analyze.visualization.plot_heatmap import execute_plot_heatmap_by_phase
 # gif
-from analyze.visualization.make_gif import make_gif, make_gif_phase, make_gif_heatmap
+from analyze.visualization.make_gif import make_gif_surface, make_gif_surface_by_phase, make_gif_heatmap_by_phase
 
 if __name__ == '__main__':
     # デバッグ1
-    conditions, exp_name = exp_016_01_00_x_set_debug(60,61)
-    execute_simulation_memory_save(conditions, exp_name)
+    select_exp_index = list(range(20))
+    selected_conditions, exp_name = exp_016_01_00_x_set_debug(select_exp_index)
+    execute_simulation_memory_save(selected_conditions)
 
-    # execute_plot_memory_save_2(exp_name)
-    # make_gif(exp_name)
+    # plotしたいexp_nameのexp_indexを指定する
+    select_plot_exp_index = [0, 1, 2]
+    execute_plot_surface(exp_name, select_plot_exp_index)
+    make_gif_surface(exp_name)
 
-    execute_plot_phase(exp_name, 100)
-    # make_gif_phase(exp_name, 100)
+    execute_plot_surface_by_phase(exp_name, 100)
+    make_gif_surface_by_phase(exp_name, 100)
 
-    # execute_plot_heatmap(exp_name, t_step=100)
-    # make_gif_heatmap(exp_name, 100)
+    execute_plot_heatmap_by_phase(exp_name, t_step=100)
+    make_gif_heatmap_by_phase(exp_name, 100)
