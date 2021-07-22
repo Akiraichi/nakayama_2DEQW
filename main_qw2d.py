ce686d92aeb8d654da541a272fa828a09cd3bdb3
aki <s2111@gm.kochi-ct.jp>
2021-07-22 19:19:42 +0900

    空白削除

--- main_qw2d.py
+++ main_qw2d.py
@@ -10,7 +10,6 @@ from analyze.visualization.plot_heatmap_by_phase import execute_plot_heatmap_by_
 from analyze.visualization.make_gif import make_gif_surface, make_gif_surface_by_phase, make_gif_heatmap_by_phase
 
 if __name__ == '__main__':
-
     select_exp_index_list = list(range(10, 21))
     print(select_exp_index_list)
     selected_conditions, exp_name = exp_016_01_00_x_set_debug(select_exp_index_list)
@@ -19,7 +18,7 @@ if __name__ == '__main__':
     # plotしたいexp_nameのexp_indexを指定する
     select_plot_exp_index = select_exp_index_list
     execute_plot_surface(exp_name=exp_name, plot_exp_index_list=select_plot_exp_index)
-    make_gif_surface(exp_name=exp_name,plot_exp_index_list=select_plot_exp_index)
+    make_gif_surface(exp_name=exp_name, plot_exp_index_list=select_plot_exp_index)
 
     execute_plot_surface_by_phase(exp_name=exp_name, plot_t_step=100)
     make_gif_surface_by_phase(exp_name=exp_name, plot_t_step=100)
