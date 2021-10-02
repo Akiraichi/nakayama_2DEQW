0da68e2a1ad0bb55c80652c286930e02c141ff89
aki <s2111@gm.kochi-ct.jp>
2021-10-02 17:50:11 +0900

    no message

--- main_qw2d.py
+++ main_qw2d.py
@@ -9,34 +9,53 @@ from analyze.visualization.plot_heatmap_by_phase import execute_plot_heatmap_by_
 # gif
 from analyze.visualization.make_gif import make_gif_surface, make_gif_surface_by_phase, make_gif_heatmap_by_phase
 
+
+def erase_eqw_simulation():
+    # 電場を消し去る場合のシミュレーション
+    # select_exp_index_list = list(range(10, 21))
+    select_exp_index_list = [3]
+    # erase_tステップ目から電場を消し去る
+    erase_t = 100
+    print(select_exp_index_list)
+    selected_conditions, exp_name = exp_017(exp_index_list=select_exp_index_list, erase_t=erase_t)
+    execute_simulation(exact_condition_list=selected_conditions)
+
+    plot_all(select_exp_index_list=select_exp_index_list, exp_name=exp_name)
+
+
+def eqw_simulation():
+    # 電場を消し去らない場合のシミュレーション
+    select_exp_index_list = [3]
+    print(select_exp_index_list)
+    selected_conditions, exp_name = exp_016_01_00_x_set(exp_index_list=select_exp_index_list)
+    execute_simulation(exact_condition_list=selected_conditions)
+    plot_all(select_exp_index_list=select_exp_index_list, exp_name=exp_name)
+
+
+def plot_all(select_exp_index_list, exp_name):
+    # plotしたいexp_nameのexp_indexを指定する
+    select_plot_exp_index = select_exp_index_list
+    execute_plot_surface(exp_name=exp_name, plot_exp_index_list=select_plot_exp_index)
+    make_gif_surface(exp_name=exp_name, plot_exp_index_list=select_plot_exp_index)
+
+    execute_plot_surface_by_phase(exp_name=exp_name, plot_t_step=100)
+    make_gif_surface_by_phase(exp_name=exp_name, plot_t_step=100)
+
+    execute_plot_heatmap_by_phase(exp_name=exp_name, plot_t_step=100)
+    make_gif_heatmap_by_phase(exp_name=exp_name, plot_t_step=100)
+
+
 if __name__ == '__main__':
-    def eqw_simulation():
-        # 電場を消し去る場合のシミュレーション
-        # select_exp_index_list = list(range(10, 21))
-        select_exp_index_list = [59]
-        # erase_tステップ目から電場を消し去る
-        erase_t = 50
-        print(select_exp_index_list)
-        selected_conditions, exp_name = exp_017(exp_index_list=select_exp_index_list, erase_t=erase_t)
-        execute_simulation(exact_condition_list=selected_conditions)
+    # 電場を消し去る場合のシミュレーション
+    # select_exp_index_list = list(range(10, 21))
+    select_exp_index_list = [3]
+    # erase_tステップ目から電場を消し去る
+    erase_t = 100
+    print(select_exp_index_list)
+    selected_conditions, exp_name = exp_017(exp_index_list=select_exp_index_list, erase_t=erase_t)
+    execute_simulation(exact_condition_list=selected_conditions)
 
-
-    def qw_simulation():
-        # 電場を消し去らない場合のシミュレーション
-        select_exp_index_list = [59]
-        print(select_exp_index_list)
-        selected_conditions, exp_name = exp_017(exp_index_list=select_exp_index_list)
-        execute_simulation(exact_condition_list=selected_conditions)
-
-
-    def plot_all(select_exp_index_list, exp_name):
-        # plotしたいexp_nameのexp_indexを指定する
-        select_plot_exp_index = select_exp_index_list
-        execute_plot_surface(exp_name=exp_name, plot_exp_index_list=select_plot_exp_index)
-        make_gif_surface(exp_name=exp_name, plot_exp_index_list=select_plot_exp_index)
-        #
-        # execute_plot_surface_by_phase(exp_name=exp_name, plot_t_step=100)
-        # make_gif_surface_by_phase(exp_name=exp_name, plot_t_step=100)
-        #
-        # execute_plot_heatmap_by_phase(exp_name=exp_name, plot_t_step=100)
-        # make_gif_heatmap_by_phase(exp_name=exp_name, plot_t_step=100)
+    # plotしたいexp_nameのexp_indexを指定する
+    select_plot_exp_index = select_exp_index_list
+    execute_plot_surface(exp_name=exp_name, plot_exp_index_list=select_plot_exp_index)
+    make_gif_surface(exp_name=exp_name, plot_exp_index_list=select_plot_exp_index)
