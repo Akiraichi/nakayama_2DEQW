b24447e945550016d6ceb8918294dc4eb7f980c2
aki <s2111@gm.kochi-ct.jp>
2021-10-06 15:11:22 +0900

    不要なコードの削除

--- analyze/visualization/plot_var.py
+++ analyze/visualization/plot_var.py
@@ -1,10 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
-import config.config
 from config.config import *
 import joblib
-from multiprocessing import Pool
 import glob
 from simulation.algorithm import calculate_probability_distribution_at_time_t_memory_save
 
@@ -21,24 +18,6 @@ def load_env_date(exp_name, i):
     len_x = env_data.len_x
     len_y = env_data.len_y
     return T, condition, len_x, len_y
-
-
-def check_plot_progress(exp_name, plot_exp_index, T):
-    # plotがどこまで進んだかをチェックし途中から再開するために、
-    # plotのindexのフォルダが既に存在しており、plot数が足りていたらそのplotはは既に終了しているとする。
-    finished = False
-    folder_path = config_plot_save_path(exp_name, plot_exp_index)
-    file_list = glob.glob(f"{folder_path}/*")
-
-    need_file_num = T + 1
-    file_num = len(file_list)
-    print(f"exp_index={plot_exp_index}のデータ数：{file_num}")
-    print(f"必要なデータ数：{need_file_num}")
-
-    if need_file_num == file_num:
-        print_green_text(f"plot_exp_index={plot_exp_index}：既に完了")
-        finished = True
-    return finished
 
 
 def execute_plot_var(exp_name, plot_exp_index_list):
@@ -79,12 +58,6 @@ def execute_plot_var(exp_name, plot_exp_index_list):
         save_data_object = joblib.load(f"{config_simulation_data_save_path(exp_name, plot_exp_index)}/000.jb")
         # 展開する
         condition = save_data_object["実験条件データ（condition）"]
-        T = condition.T
-
-        # plot_exp_indexのプロットが既に終了しているのかをチェックする。完了していたらスキップする
-        # if check_plot_progress(exp_name, plot_exp_index, T):
-        #     continue
-
         print(f"START：分散を求める処理：plot_exp_index={plot_exp_index}")
 
         """
@@ -123,43 +96,7 @@ def execute_plot_var(exp_name, plot_exp_index_list):
             var_y_list.append(var_y[200])
             var_all_list.append(var_all)
             t_list.append(t_step)
-
         do_plot_var(exp_name, var_x_list, var_y_list, var_all_list, t_list, plot_exp_index)
-
-
-def plot_dist_and_save(simulation_data_file_name, plot_path):
-    """
-    :param simulation_data_file_name: plotするデータファイルの名前（exp_nameのexp_indexに入っている）
-    :param plot_path: plotした画像の保存場所
-    :return:
-    """
-    # データをロード
-    save_data_object = joblib.load(simulation_data_file_name)
-    # 展開
-    condition = save_data_object["実験条件データ（condition）"]
-    T = condition.T
-    len_x = 2 * T + 1
-    len_y = 2 * T + 1
-    t = save_data_object["このシミュレーションデータが何ステップ目か（t）"]
-    PSY = save_data_object["シミュレーションデータ"]
-
-    # 実験データを手に入れたので、ここから分散を求めていく
-
-    # # 格子点を作成
-    # mesh_x, mesh_y = np.meshgrid(np.linspace(-T, T, 2 * T + 1), np.linspace(-T, T, 2 * T + 1), indexing="ij")
-    # 確率の計算。probability[x,y]で全座標の確率が格納される
-    probability = calculate_probability_distribution_at_time_t_memory_save(PSY, len_x, len_y)
-    prob_y = np.var(probability, axis=0)  # 行ごとの分散を求める。y軸方向の分散が求まる
-    prob_x = np.var(probability, axis=1)  # 列ごとの分散を求める。x軸方向の分散が求まる
-
-    # 0埋めする
-    t_index = str(t).zfill(3)
-    # タイトルを設定
-    title = f"$t={t}$"
-    # plotする
-
-    # do_plot_3d_gif(mesh_x, mesh_y, mesh_z, plot_path, f"{t_index}.png", title)
-    print(f"t={t_index}：分散の可視化と保存：完了")
 
 
 def do_plot_var(exp_name, var_x_list, var_y_list, var_all_list, t_list, plot_exp_index):
@@ -168,22 +105,3 @@ def do_plot_var(exp_name, var_x_list, var_y_list, var_all_list, t_list, plot_exp
     ax.scatter(t_list, var_all_list, c='blue')
     fig.savefig(f'{config_var_save_path(exp_name=exp_name)}/{plot_exp_index}.png')
     print(plot_exp_index)
-
-
-def do_plot_3d_gif(mesh_x, mesh_y, mesh_z, path, file_name, title):
-    fig = plt.figure(figsize=(4, 3), tight_layout=True, dpi=400)
-    ax = fig.add_subplot(1, 1, 1, projection='3d')
-    ax.set_xlabel("$x$", size=24, labelpad=10)
-    ax.set_ylabel("$y$", size=24)
-    ax.set_zlabel("$p$", size=24, labelpad=15)
-    # 軸メモリの調整
-    ax.tick_params(labelsize=24)
-    # タイトルを設定
-    ax.set_title(title, size=24)
-    # 曲面を描画
-    ax.plot_surface(mesh_x, mesh_y, mesh_z, cmap="summer")
-    plt.savefig(f"{path}/{file_name}", dpi=400, bbox_inches='tight')
-    # メモリ解放
-    fig.clf()
-    ax.cla()
-    plt.close()
