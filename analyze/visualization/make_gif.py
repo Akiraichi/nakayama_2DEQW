c8869a6ba13752987803d523b51a0a13269cfc2d
aki <s2111@gm.kochi-ct.jp>
2021-10-12 17:39:11 +0900

    make_gif_surfaceをクラスを使って、可読性を向上。

--- analyze/visualization/make_gif.py
+++ analyze/visualization/make_gif.py
@@ -10,42 +10,63 @@ def check_gif_progress(exp_name, exp_index):
     # plotのexp_indexのフォルダが既に存在しており、plot数が足りていたらそのplotはは既に終了しているとする。
     finished = False
     # ファイルが存在するかをチェックする
-    if os.path.isfile(f'{config_marge_gif_save_path_file_name(exp_name=exp_name)}/{str(exp_index).zfill(2)}.gif'):
+    if os.path.isfile(f'{config_gif_save_path_file_name(exp_name=exp_name)}/{str(exp_index).zfill(2)}.gif'):
         finished = True
         print_green_text(f"exp_index={exp_index}：既に完了")
 
     return finished
 
 
-def make_gif_surface(exp_name, plot_exp_index_list, duration=50):
-    """
-    exp_nameのplot_exp_index_listに含まれたからexp_indexからgifを作成する
-    :param plot_exp_index_list:
-    :param exp_name:
-    :param duration:
-    :return:
-    """
-    for exp_index in plot_exp_index_list:
-        # gifをどこまで作成したかチェックし続きから実行する
-        if check_gif_progress(exp_name, exp_index):
-            continue
+class Make_gif:
+    def __init__(self):
+        self.exp_name = None
+        self.plot_exp_index = None
+        self.duration = None
 
-        print(f"START：exp_index={exp_index}")
+    def set_up_conditions(self, exp_name, plot_exp_index, duration):
+        self.exp_name = exp_name
+        self.plot_exp_index = plot_exp_index
+        self.duration = duration
+
+    def check_gif_progress(self):
+        # plotがどこまで進んだかをチェックし途中から再開するために、
+        # plotのexp_indexのフォルダが既に存在しており、plot数が足りていたらそのplotはは既に終了しているとする。
+        finished = False
+        # ファイルが存在するかをチェックする
+        if os.path.isfile(
+                f'{config_gif_save_path_file_name(exp_name=self.exp_name)}/{str(self.plot_exp_index).zfill(2)}.gif'):
+            finished = True
+            print_green_text(f"exp_index={self.plot_exp_index}：既に完了")
+
+        return finished
+
+    def plot_surface(self):
+        print(f"START：exp_index={self.plot_exp_index}")
         # 全ファイル名を取得
-        file_name_list = glob.glob(f"{config_plot_save_path(exp_name=exp_name, index=exp_index)}/*")
+        file_name_list = glob.glob(f"{config_plot_save_path(exp_name=self.exp_name, index=self.plot_exp_index)}/*")
         file_name_list.sort()
 
         frames = []
         for file_name in file_name_list:
             new_frame = Image.open(file_name)
             frames.append(new_frame)
-        frames[0].save(f'{config_marge_gif_save_path_file_name(exp_name=exp_name)}/{str(exp_index).zfill(2)}.gif',
-                       format='GIF',
-                       append_images=frames[1:],
-                       save_all=True,
-                       duration=duration,
-                       loop=0)
-    print_finish("make_gif_surface")
+        frames[0].save(
+            f'{config_gif_save_path_file_name(exp_name=self.exp_name)}/{str(self.plot_exp_index).zfill(2)}.gif',
+            format='GIF',
+            append_images=frames[1:],
+            save_all=True,
+            duration=self.duration,
+            loop=0)
+        print_finish("make_gif_surface")
+
+
+def make_gif_surface(exp_name, plot_exp_index_list, duration=50):
+    for plot_exp_index in plot_exp_index_list:
+        plotter = Make_gif()
+        if plotter.check_gif_progress():
+            continue
+        plotter.set_up_conditions(exp_name=exp_name, plot_exp_index=plot_exp_index, duration=duration)
+        plotter.plot_surface()
 
 
 def make_gif_surface_by_phase(exp_name, plot_t_step, duration=100):
