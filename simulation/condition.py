a47f4f6699a61cd09e052c3d082116630e9061c3
中山昌勲 <>
2021-12-30 17:13:27 +0900

    conditionをより保守性の高いコードへ改善。

--- simulation/condition.py
+++ simulation/condition.py
@@ -1,162 +1,192 @@
 import numpy as np
-from config.config import ConfigSimulation
+from config.config import ConfigSimulation, print_warning
 
 
 class Condition:
-    def __init__(self):
-        self.T = None
-        self.P = None
-        self.Q = None
-        self.R = None
-        self.S = None
-        self.PSY_init = None
-        self.algorithm = None
-        self.phi = None
-        self.phi_latex = None
-        self.exp_name = None
-        self.exp_index = None
-        # numbaでコンパイルする際に、型をi8にしているので、初期値は0とする。
-        self.erase_t = 0
-        # 電場をどの程度ゆっくり消すか
-        self.erase_time_step = 0
+    def __init__(self, **kwargs):
+        self.__data = kwargs
+
+    @classmethod
+    def prepare(cls, pattern, options=None):
+        """
+
+        Args:
+            pattern: ConditionType から一つ指定する
+            options: プロパティの上書きしたいものを指定する
+
+        Returns: Condition Instanceを返す
+
+        """
+        if options is None:
+            options = {}
+
+        if pattern == ConditionType.Grover:
+            return cls(**{**DefaultGroverProps, **options})  # https://note.nkmk.me/python-dict-add-update/
+        elif pattern == ConditionType.Hadamard:
+            return cls(**{**DefaultHadamardProps, **options})
+        elif pattern == ConditionType.DFT:
+            return cls(**{**DefaultDFTProps, **options})
+        else:
+            print_warning("Conditionの設定がおかしいですよ！")
+            raise OSError
+
+    @property
+    def T(self):
+        return self.__data["T"]
+
+    @property
+    def P(self):
+        return self.__data["P"]
+
+    @property
+    def Q(self):
+        return self.__data["Q"]
+
+    @property
+    def R(self):
+        return self.__data["R"]
+
+    @property
+    def S(self):
+        return self.__data["S"]
+
+    @property
+    def PSY_init(self):
+        return self.__data["PSY_init"]
+
+    @property
+    def algorithm(self):
+        return self.__data["algorithm"]
+
+    @property
+    def phi(self):
+        return self.__data["phi"]
+
+    @property
+    def phi_latex(self):
+        return self.__data["phi_latex"]
+
+    @property
+    def exp_name(self):
+        return self.__data["exp_name"]
+
+    @property
+    def exp_index(self):
+        return self.__data["exp_index"]
+
+    @property
+    def erase_t(self):
+        return self.__data["erase_t"]
+
+    @property
+    def erase_time_step(self):
+        return self.__data["erase_time_step"]
 
 
-def set_grover_condition(condition):
-    # シミュレーション条件
-    condition.PSY_init = None
-    condition.T = ConfigSimulation.MaxTimeStep  # 最大時間発展T
-    condition.P = np.array([
+class ConditionType:
+    Grover = "Grover"
+    Hadamard = "Hadamard"
+    DFT = "DFT"
+
+
+DefaultBaseProps = {
+    "T": ConfigSimulation.MaxTimeStep,
+    "PSY_init": None,
+    "algorithm": None,
+    "phi": None,
+    "phi_latex": None,
+    "exp_name": None,
+    "exp_index": None,
+    # numbaでコンパイルする際に、型をi8にしているので、初期値は0とする。
+    "erase_t": 0,
+    # 電場をどの程度ゆっくり消すか
+    "erase_time_step": 0
+}
+
+DefaultGroverProps = {
+    **DefaultBaseProps,
+    "P": np.array([
         [-1 / 2, 1 / 2, 1 / 2, 1 / 2],
         [0, 0, 0, 0],
         [0, 0, 0, 0],
         [0, 0, 0, 0]
-    ], dtype=np.complex128)
-    condition.Q = np.array([
+    ], dtype=np.complex128),
+    "Q": np.array([
         [0, 0, 0, 0],
         [1 / 2, -1 / 2, 1 / 2, 1 / 2],
         [0, 0, 0, 0],
         [0, 0, 0, 0]
-    ], dtype=np.complex128)
-    condition.R = np.array([
+    ], dtype=np.complex128),
+    "R": np.array([
         [0, 0, 0, 0],
         [0, 0, 0, 0],
         [1 / 2, 1 / 2, -1 / 2, 1 / 2],
         [0, 0, 0, 0]
-    ], dtype=np.complex128)
-    condition.S = np.array([
+    ], dtype=np.complex128),
+    "S": np.array([
         [0, 0, 0, 0],
         [0, 0, 0, 0],
         [0, 0, 0, 0],
         [1 / 2, 1 / 2, 1 / 2, -1 / 2]
     ], dtype=np.complex128)
+}
 
-
-def set_hadamard_condition(condition):
-    # シミュレーション条件
-    condition.PSY_init = None
-    condition.T = ConfigSimulation.MaxTimeStep  # 最大時間発展T
-    condition.P = np.array([
+DefaultHadamardProps = {
+    **DefaultBaseProps,
+    "P": np.array([
         [1 / 2, 1 / 2, 1 / 2, 1 / 2],
         [0, 0, 0, 0],
         [0, 0, 0, 0],
         [0, 0, 0, 0]
-    ], dtype=np.complex128)
-    condition.Q = np.array([
+    ], dtype=np.complex128),
+    "Q": np.array([
         [0, 0, 0, 0],
         [1 / 2, -1 / 2, 1 / 2, -1 / 2],
         [0, 0, 0, 0],
         [0, 0, 0, 0]
-    ], dtype=np.complex128)
-    condition.R = np.array([
+    ], dtype=np.complex128),
+    "R": np.array([
         [0, 0, 0, 0],
         [0, 0, 0, 0],
         [1 / 2, -1 / 2, -1 / 2, 1 / 2],
         [0, 0, 0, 0]
-    ], dtype=np.complex128)
-
-    condition.S = np.array([
+    ], dtype=np.complex128),
+    "S": np.array([
         [0, 0, 0, 0],
         [0, 0, 0, 0],
         [0, 0, 0, 0],
         [1 / 2, 1 / 2, -1 / 2, -1 / 2]
     ], dtype=np.complex128)
-    # condition.P = np.array([
-    #     [1 / 2, 1 / 2, 1 / 2, 1 / 2],
-    #     [0, 0, 0, 0],
-    #     [0, 0, 0, 0],
-    #     [0, 0, 0, 0]
-    # ], dtype=np.complex128)
-    # condition.Q = np.array([
-    #     [0, 0, 0, 0],
-    #     [1 / 2, -1 / 2, 1 / 2, -1 / 2],
-    #     [0, 0, 0, 0],
-    #     [0, 0, 0, 0]
-    # ], dtype=np.complex128)
-    # condition.R = np.array([
-    #     [0, 0, 0, 0],
-    #     [0, 0, 0, 0],
-    #     [1 / 2, 1 / 2, -1 / 2, -1 / 2],
-    #     [0, 0, 0, 0]
-    # ], dtype=np.complex128)
-    #
-    # condition.S = np.array([
-    #     [0, 0, 0, 0],
-    #     [0, 0, 0, 0],
-    #     [0, 0, 0, 0],
-    #     [1 / 2, -1 / 2, -1 / 2, 1 / 2]
-    # ], dtype=np.complex128)
+}
 
-
-def set_DFT_condition(condition):
-    # シミュレーション条件
-    condition.PSY_init = None
-    condition.T = ConfigSimulation.MaxTimeStep  # 最大時間発展T
-    condition.P = np.array([
+DefaultDFTProps = {
+    **DefaultBaseProps,
+    "P": np.array([
         [1 / 2, 1 / 2, 1 / 2, 1 / 2],
         [0, 0, 0, 0],
         [0, 0, 0, 0],
         [0, 0, 0, 0]
-    ], dtype=np.complex128)
-    condition.Q = np.array([
+    ], dtype=np.complex128),
+    "Q": np.array([
         [0, 0, 0, 0],
         [1 / 2, -1j / 2, -1 / 2, 1j / 2],
         [0, 0, 0, 0],
         [0, 0, 0, 0]
-    ], dtype=np.complex128)
-    condition.R = np.array([
+    ], dtype=np.complex128),
+    "R": np.array([
         [0, 0, 0, 0],
         [0, 0, 0, 0],
         [1 / 2, -1 / 2, 1 / 2, -1 / 2],
         [0, 0, 0, 0]
-    ], dtype=np.complex128)
-    condition.S = np.array([
+    ], dtype=np.complex128),
+    "S": np.array([
         [0, 0, 0, 0],
         [0, 0, 0, 0],
         [0, 0, 0, 0],
         [1 / 2, 1j / 2, -1 / 2, -1j / 2]
     ], dtype=np.complex128)
+}
 
-    # condition.P = np.array([
-    #     [1 / 2, 1 / 2, 1 / 2, 1 / 2],
-    #     [0, 0, 0, 0],
-    #     [0, 0, 0, 0],
-    #     [0, 0, 0, 0]
-    # ], dtype=np.complex128)
-    # condition.Q = np.array([
-    #     [0, 0, 0, 0],
-    #     [1 / 2, 1j / 2, -1 / 2, -1j / 2],
-    #     [0, 0, 0, 0],
-    #     [0, 0, 0, 0]
-    # ], dtype=np.complex128)
-    # condition.R = np.array([
-    #     [0, 0, 0, 0],
-    #     [0, 0, 0, 0],
-    #     [1 / 2, -1 / 2, 1 / 2, -1 / 2],
-    #     [0, 0, 0, 0]
-    # ], dtype=np.complex128)
-    # condition.S = np.array([
-    #     [0, 0, 0, 0],
-    #     [0, 0, 0, 0],
-    #     [0, 0, 0, 0],
-    #     [1 / 2, -1j / 2, -1 / 2, 1j / 2]
-    # ], dtype=np.complex128)
+if __name__ == '__main__':
+    c = Condition.prepare(pattern=ConditionType.Grover, options={"phi": 49})
+    print(type(c))
