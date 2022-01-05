90a1f8fa91a9ea5731f787e78875480ee0924d09
中山昌勲 <>
2022-01-05 18:51:21 +0900

    インターフェースが作成完了

--- simulation_result_analyzer_and_plotter.py
+++ simulation_result_analyzer_and_plotter.py
@@ -28,9 +28,14 @@ class SimulationResultAnalyzer:
         analyzer_ = Analyzer(self.__qw1, self.__qw2, self.__analyze_indexes, self.__options)
         analyzer_.start_processing()
 
-    def plot(self):
-        plotter = AnalyzePlotter(self.__qw1, self.__qw2, self.__analyze_indexes)
-        plotter.plot_x_axis_is_t(plot_t_list=[50, 100, 200])
+    def plot_x_axis_is_index(self, plot_t_list):
+        plotter = AnalyzePlotter(self.__qw1, self.__qw2, self.__analyze_indexes, plot_t_list, x_axis="index")
+        plotter.plot()
+
+    def plot_x_axis_is_t(self, plot_indexes, start_t=0):
+        plotter = AnalyzePlotter(self.__qw1, self.__qw2, self.__analyze_indexes, plot_indexes, x_axis="t",
+                                 start_t=start_t)
+        plotter.plot()
 
 
 if __name__ == '__main__':
@@ -40,4 +45,7 @@ if __name__ == '__main__':
                                         analyze_indexes=indexes,
                                         options={"parallel": True})
     # analyzer.analyze()
-    analyzer.plot()
+    t_list = [10, 20, 50, 100]
+    analyzer.plot_x_axis_is_index(plot_t_list=t_list)
+    # plot_indexes = [50, 100]
+    # analyzer.plot_x_axis_is_t(plot_indexes=plot_indexes, start_t=50)
