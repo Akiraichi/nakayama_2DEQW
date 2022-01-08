25285db0546ec7b604bc05c6955b747ce80aade3
中山昌勲 <>
2022-01-08 22:48:33 +0900

    新しく複数の関数を追加した

--- helper.py
+++ helper.py
@@ -1,16 +1,20 @@
 import glob
 import sympy
-
+import matplotlib.pyplot as plt
 import joblib
 import zlib
-from config.config import ConfigSimulation, config_simulation_data_save_path, print_warning
+import termcolor
+from typing import List
+
+from config.config_simulation import ConfigSimulationSetting, config_simulation_data_save_path
+# from config.config_data_analyze import DefaultOptimizePlotSetting
 from simulation.simulation_core import calc_probability
 
 
 def return_simulation_data_file_names(exp_name, exp_index):
     """
     GoogleDriveで、うまく動作するように、チェック処理を追加している。
-    return: exp_nameのplot_exp_index以下にある全jbファイルのパスのリストを返却する。この際、
+    return: exp_nameのexp_index以下にある全jbファイルのパスのリストを返却する。この際、
     max_time_stepとファイル数を比較することで全て抽出できているかをチェックする
     """
     count = 0
@@ -18,7 +22,7 @@ def return_simulation_data_file_names(exp_name, exp_index):
         simulation_data_file_names = glob.glob(
             f"{config_simulation_data_save_path(exp_name=exp_name, str_t=None, index=exp_index)}**/*.jb")
         simulation_data_file_names.sort()  # 実験順にsortする。
-        if len(simulation_data_file_names) == ConfigSimulation.MaxTimeStep + 1:
+        if len(simulation_data_file_names) == ConfigSimulationSetting.MaxTimeStep + 1:
             # シミュレーションデータ全てのpathをgrobできているかのチェック
             break
         count += 1
@@ -30,7 +34,7 @@ def return_simulation_data_file_names(exp_name, exp_index):
     return simulation_data_file_names
 
 
-def load_data_by_error_handling(file_path):
+def load_file_by_error_handling(file_path):
     while True:
         try:
             data = joblib.load(file_path)
@@ -69,7 +73,7 @@ def save_jb_file(data_dict, folder_path, file_name):
 
 
 def get_probability(simulation_data_file_names, index):
-    save_data_object = load_data_by_error_handling(simulation_data_file_names[index])
+    save_data_object = load_file_by_error_handling(simulation_data_file_names[index])
     # save_data_object = joblib.load(simulation_data_file_names[index])
     condition = save_data_object["実験条件データ（condition）"]
 
@@ -86,3 +90,179 @@ def get_probability(simulation_data_file_names, index):
     # probability[x,y]として(x,y)座標の確率を求められる。
     probability = calc_probability(PSY, len_x, len_y)
     return probability
+
+
+# TODO:直す
+# def plot_multi_scatter(setting: List[DefaultOptimizePlotSetting]):
+#     x_label = setting[0].x_label
+#     y_label = setting[0].y_label
+#     title = setting[0].title
+#     file_name = setting[0].file_name
+#     path_to_file = setting[0].path_to_file
+#     x_axis_data = setting[0].x_axis_data_list
+#     data_num = len(setting[0].y_axis_data_list)
+#
+#     # Figureの初期化
+#     fig = plt.figure(figsize=(8, 6))
+#     # axオブジェクトの生成
+#     ax = fig.add_subplot(111)
+#     ax.set_xlabel(f"${x_label}$", size=24, labelpad=5)
+#     ax.set_ylabel(f"${y_label}$", size=24)
+#     ax.set_title(title)  # グラフタイトル
+#     # 描画
+#     for i in range(data_num):
+#         y_axis_data_ = [y_axis[0] for y_axis in y_axis_data]
+#         ax.scatter(x_axis_data, y_axis_data_, s=10, label=f"${x_label}={x_axis_data}$")
+#
+#     plt.legend()
+#     plt.savefig(f"{path_to_file}/{file_name}.png", dpi=400)
+#     ax.cla()
+#     plt.close()
+
+
+def check_finished_file(folder_path: str, will_generate_index_list: list, extension: str):
+    """
+    処理が完了しているかを確認し、処理できていないファイルのみ処理する。
+    """
+    # すでに生成されたファイルのファイル名の一覧を取得する
+    generated_file_names = glob.glob(f"{folder_path}/*.{extension}")
+
+    # すでに生成されたファイルのindexの一覧を求める
+    generated_index_list = [extract_index_from_file_name(generated_file_name, extension) for generated_file_name in
+                            generated_file_names]
+
+    # 共通しない要素のうち、まだ処理されていないものを取得
+    not_generated_index_list = set(will_generate_index_list) - set(generated_index_list)
+
+    # 実行状況に応じて、状況を報告
+    if not_generated_index_list:
+        for not_generated_index in not_generated_index_list:
+            print_warning(f"index={not_generated_index}：完了していません")
+    else:
+        for generated_index in generated_index_list:
+            print_green_text(f"index={generated_index}：既に完了")
+
+    return not_generated_index_list
+
+
+def extract_index_from_file_name(file_name, extension):
+    """
+    abcde_0000.jbのようなファイルにおいて、0000を抽出する
+    Args:
+        file_name: file名
+        extension: fileの拡張子
+
+    Returns:
+
+    """
+    len_extension = len(extension)  # 拡張子の文字数
+    len_index = 4  # example 0000.jb の場合indexは4文字
+    index = int(file_name[-(len_extension + 1 + len_index):-(len_extension + 1)])
+    return index
+
+
+def print_warning(text):
+    warning = '*' * 30 + '\n'
+    warning += '*{:^28}*\n'.format('Warning')
+    warning += '*' * 30 + '\n'
+    colored_warning = termcolor.colored(warning, 'red')
+    print(colored_warning)
+    print(termcolor.colored(text, 'red'))
+
+
+def print_finish(text):
+    notice = '-' * 30 + '\n'
+    notice += '|{:^28}|\n'.format(f'Finish：{text}')
+    notice += '-' * 30 + '\n'
+    colored_warning = termcolor.colored(notice, 'green')
+    print(colored_warning)
+
+
+def print_green_text(text):
+    colored_text = termcolor.colored(text, 'green')
+    print(colored_text)
+
+
+def check_simulation_data(obj_a, obj_b):
+    """
+    Example:
+    obj_a=[exp_name, exp_index]
+    obj_b=[exp_name, exp_index]
+    obj_aとobj_bのシミュレーションデータが完全に一致するかチェックします
+    :param obj_a:env使わない方。古い方
+    :param obj_b:
+    :return:
+    """
+    exp_index_a_list = obj_a[0]
+    exp_name_a = obj_a[1]
+
+    exp_index_b_list = obj_b[0]
+    exp_name_b = obj_b[1]
+
+    for k in range(len(exp_index_b_list)):
+        # データをロードする
+        # exp_nameにある全実験フォルダーへのpath。sortもする。
+        simulation_data_file_names_a = glob.glob(
+            f"{config_simulation_data_save_path(exp_name_a, exp_index_a_list[k])}/*.jb")
+        simulation_data_file_names_a.sort()  # 実験順にsortする。
+
+        simulation_data_file_names_b = glob.glob(
+            f"{config_simulation_data_save_path(exp_name_b, exp_index_b_list[k])}/*.jb")
+        simulation_data_file_names_b.sort()  # 実験順にsortする。
+        T = len(simulation_data_file_names_a) - 1
+        for t in range(T + 1):
+            psy_a_obj = joblib.load(simulation_data_file_names_a[t])
+            psy_a = psy_a_obj["シミュレーションデータ"]
+            psy_b = joblib.load(simulation_data_file_names_b[t])
+            # 各成分をチェックする
+            for x in range(2 * T + 1):
+                for y in range(2 * T + 1):
+                    for i in range(4):
+                        right = psy_a[x, y, i]
+                        test = psy_b[x, y, i]
+                        if right != test:
+                            print_warning(f"異なる：t={t}：x={x}：y={y}：right - test={right - test}")
+            print_green_text(f"t={t}回目終わり")
+        print_green_text(f"k={k}回目終わり")
+
+
+def select_plot_t_step():
+    # どのデータ抽出するかを選択する
+    t_list = None
+    if ConfigSimulationSetting.MaxTimeStep == 2000:
+        t_list = list(range(0, 2020, 20))
+    elif ConfigSimulationSetting.MaxTimeStep == 600:
+        t_list = list(range(0, 605, 5))
+    elif ConfigSimulationSetting.MaxTimeStep == 200:
+        t_list = list(range(0, 205, 5))
+    elif ConfigSimulationSetting.MaxTimeStep == 100:
+        t_list = list(range(0, 105, 5))
+    return t_list
+
+
+def select_plot_t_step_detail():
+    # どのデータ抽出するかを選択する
+    t_list = None
+    if ConfigSimulationSetting.MaxTimeStep == 2000:
+        t_list = list(range(0, 2005, 5))
+    elif ConfigSimulationSetting.MaxTimeStep == 600:
+        t_list = list(range(0, 605, 5))
+    elif ConfigSimulationSetting.MaxTimeStep == 200:
+        t_list = list(range(0, 205, 5))
+    elif ConfigSimulationSetting.MaxTimeStep == 100:
+        t_list = list(range(0, 105, 5))
+    return t_list
+
+
+def select_plot_t_step_by_100():
+    # どのデータ抽出するかを選択する
+    t_list = None
+    if ConfigSimulationSetting.MaxTimeStep == 2000:
+        t_list = list(range(0, 2100, 100))
+    elif ConfigSimulationSetting.MaxTimeStep == 600:
+        t_list = [100, 200, 300, 400, 500, 600]
+    elif ConfigSimulationSetting.MaxTimeStep == 200:
+        t_list = list(range(0, 205, 5))
+    elif ConfigSimulationSetting.MaxTimeStep == 100:
+        t_list = list(range(0, 105, 5))
+    return t_list
