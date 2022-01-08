4fa7c28279fe58190513d1a437dcf756744a88d5
中山昌勲 <>
2022-01-08 22:45:23 +0900

    シミュレーションに関係する設定を保存するファイルを作成した。 dataclassを使ってコードを書き直した

---
+++ config/config_simulation.py
@@ -0,0 +1,46 @@
+import os
+import sys
+from dataclasses import dataclass, field
+
+
+@dataclass(frozen=True)
+class CloudSimulationSetting:
+    MaxTimeStep: int = 600
+    SimulationParallelNum: int = 4
+    PlotParallelNum: int = 4
+
+
+@dataclass(frozen=True)
+class LocalSimulationSetting:
+    MaxTimeStep: int = 200
+    SimulationParallelNum: int = 4
+    PlotParallelNum: int = 4
+
+
+@dataclass(frozen=True)
+class ConfigSimulationSetting:
+    if 'google.colab' in sys.modules:
+        # google colab環境
+        MaxTimeStep: int = field(init=False, default=CloudSimulationSetting.MaxTimeStep)
+        SimulationParallelNum: int = field(init=False, default=CloudSimulationSetting.SimulationParallelNum)
+        PlotParallelNum: int = field(init=False, default=CloudSimulationSetting.PlotParallelNum)
+    else:
+        # local環境
+        MaxTimeStep: int = field(init=False, default=LocalSimulationSetting.MaxTimeStep)
+        SimulationParallelNum: int = field(init=False, default=LocalSimulationSetting.SimulationParallelNum)
+        PlotParallelNum: int = field(init=False, default=LocalSimulationSetting.PlotParallelNum)
+
+
+def config_simulation_data_save_path(exp_name, str_t=None, index=None):
+    # 実験データの保存先のフォルダーがなければ作成する
+    if index is None:
+        path = f"result/{exp_name}/simulation_data_{exp_name}/"
+    elif str_t is None:
+        index = str(index).zfill(2)
+        path = f"result/{exp_name}/simulation_data_{exp_name}/{index}/"
+    else:
+        # index=実験した時の順番でつけた番号。0埋めする
+        index = str(index).zfill(2)
+        path = f"result/{exp_name}/simulation_data_{exp_name}/{index}/{str_t[:2]}/"
+    os.makedirs(path, exist_ok=True)
+    return path
