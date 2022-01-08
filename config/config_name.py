9b6fe3f34a0f46f06032d2f91b4ae6b4f9eb44b0
中山昌勲 <>
2022-01-08 22:45:59 +0900

    基本的な名前の設定に関係する設定を保存するファイルを作成した。

---
+++ config/config_name.py
@@ -0,0 +1,15 @@
+from dataclasses import InitVar, dataclass, field
+
+
+@dataclass(frozen=True)
+class DefaultNameSetting:
+    exp1_name: InitVar[str]
+    exp1_index: InitVar[int]
+    exp2_name: InitVar[str]
+    default_folder_name: str = field(init=False)
+    default_file_name: str = field(init=False)
+    default_top_folder_name: str = field(init=False, default="result")
+
+    def __post_init__(self, exp1_name: str, exp1_index: int, exp2_name: str):
+        object.__setattr__(self, "default_folder_name", f'{exp1_name}_{exp1_index}_{exp2_name}')
+        object.__setattr__(self, "default_file_name", f'{exp1_name}_{exp1_index}_{exp2_name}')
