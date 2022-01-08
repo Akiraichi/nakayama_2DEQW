a82085f5954a555d63df534e0354b1a1e72835ee
中山昌勲 <>
2022-01-08 22:50:35 +0900

    フォルダ変更に伴う変更

---
+++ data_analysis/visualization/analyze_prob.py
@@ -0,0 +1,338 @@
+# config
+import numpy as np
+
+# misc
+import joblib
+from numba import njit
+from data_analysis.data_analyze.analyzer import get_probability
+from config.config_simulation import ConfigSimulationSetting
+from helper import print_finish, print_warning, return_simulation_data_file_names, select_plot_t_step
+
+
+def main_analyze(exp_name, exp_indexes, cut_circle_r, circle_inner_r, circle_outer_r, ext):
+    analyzer = Analyzer(exp_name, exp_indexes, cut_circle_r, circle_inner_r, circle_outer_r)
+    if ext == "prob":
+        analyzer.analyze_prob()  # 確率の総和を解析
+    elif ext == "var":
+        analyzer.analyze_var()  # 確率の分散を解析
+    elif ext == "width":
+        analyzer.analyze_width()  # 確率の分布幅を解析
+    elif ext == "all":
+        analyzer.analyze_all()  # 全部を実行
+    else:
+        print_warning("該当するものがありません")
+
+
+class Analyzer:
+    def __init__(self, exp_name, exp_indexes, cut_circle_r, circle_inner_r, circle_outer_r):
+        self.exp_name = exp_name
+        self.exp_indexes = exp_indexes
+        self.cut_circle_r = cut_circle_r
+        self.circle_inner_r = circle_inner_r
+        self.circle_outer_r = circle_outer_r
+
+    def analyze_all(self):
+        for i, exp_index in enumerate(self.exp_indexes):
+            analyzer = AnalyzerCore(self.exp_name, exp_index, self.cut_circle_r, self.circle_inner_r,
+                                    self.circle_outer_r)
+            analyzer.analyze_probability()
+            analyzer.save_prob_csv_file()
+            print("確率計算完了")
+
+            analyzer.analyze_var()
+            analyzer.save_var_csv_file()
+            print("分散計算完了")
+
+            analyzer.analyze_width()
+            analyzer.save_width_csv_file()
+            print("分布幅計算完了")
+        print_finish("全体完了")
+
+    def analyze_prob(self):
+        for i, exp_index in enumerate(self.exp_indexes):
+            analyzer = AnalyzerCore(self.exp_name, exp_index, self.cut_circle_r, self.circle_inner_r,
+                                    self.circle_outer_r)
+            analyzer.analyze_probability()
+            analyzer.save_prob_csv_file()
+        print_finish("確率計算")
+
+    def analyze_var(self):
+        """
+        中心付近の確率分布を半径＝rの円の内部とする。
+        中心付近の確率分布において確率の分散を求める
+        """
+        for i, exp_index in enumerate(self.exp_indexes):
+            analyzer = AnalyzerCore(self.exp_name, exp_index, self.cut_circle_r, self.circle_inner_r,
+                                    self.circle_outer_r)
+            analyzer.analyze_var()
+            analyzer.save_var_csv_file()
+        print_finish("分散の計算")
+
+    def analyze_width(self):
+        for i, exp_index in enumerate(self.exp_indexes):
+            analyzer = AnalyzerCore(self.exp_name, exp_index, self.cut_circle_r, self.circle_inner_r,
+                                    self.circle_outer_r)
+            analyzer.analyze_width()
+            analyzer.save_width_csv_file()
+        print_finish("確率分布幅の計算")
+
+
+class AnalyzerCore:
+    def __init__(self, exp_name, exp_index, cut_circle_r, circle_inner_r, circle_outer_r):
+        # 共通設定
+        self.exp_name = exp_name
+        self.exp_index = str(exp_index).zfill(4)
+        self.cut_circle_r = cut_circle_r
+        self.t_list = select_plot_t_step()
+
+        # probの固有設定
+        self.circle_inner_r = circle_inner_r
+        self.circle_outer_r = circle_outer_r
+        self.p_in_circle_list = []
+        self.p_out_circle_list = []
+        self.p_circle_list = []
+
+        # varの固有設定
+        self.var_p_list = []  # 中心付近の確率分布の確率分布
+
+        # widthの固有設定
+        # self.X_width_center_list = []  # 中心付近の確率分布の確率幅
+        # self.Y_width_center_list = []  # 中心付近の確率分布の確率幅
+        self.std_prob_width_list = []  # 円周付近の確率分布の確率幅の標準偏差
+        self.std_prob_x_left_list = []  # 円周付近の確率分布の確率幅の標準偏差
+        self.std_prob_x_right_list = []  # 円周付近の確率分布の確率幅の標準偏差
+
+        # 関数定義された設定
+        self.__set_data_names()
+        self.__set_plot_option()
+        if ConfigSimulationSetting.MaxTimeStep == 2000:
+            pass  # 2000の場合は事前にデータをロードせず、個別にロード
+        else:
+            self.__load_data()  # データをロード
+
+    def __set_data_names(self):
+        self.simulation_data_names = return_simulation_data_file_names(exp_name=self.exp_name,
+                                                                       exp_index=int(self.exp_index))
+        # self.simulation_data_names = glob.glob(
+        #     f"{config_simulation_data_save_path(self.exp_name, int(self.exp_index))}/*.jb")
+        # self.simulation_data_names.sort()  # 実験順にsortする。
+        print(f"exp_nameのデータ数：{len(self.simulation_data_names)}")
+
+    def __set_plot_option(self):
+        # 保存先の設定
+        folder_name = f'cut_r={self.cut_circle_r}_inner_r={self.circle_inner_r}_outer_r={self.circle_outer_r}_{self.exp_name}'
+        self.save_prob_csv_path = config_prob_save_path(folder_name=folder_name)
+        self.save_var_csv_path = config_var_save_path(folder_name=folder_name)
+        self.save_width_csv_path = config_width_save_path(folder_name=folder_name)
+
+        # ファイル名を設定する
+        self.file_name = folder_name + f"_{self.exp_index}"
+
+        # プロットのタイトルを設定する
+        # title設定のために一つデータをロードする。
+        condition = joblib.load(self.simulation_data_names[0])["実験条件データ（condition）"]
+        self.title = f"{self.exp_name}" + " " + "$t_{erase}$" + f"={condition.erase_t}"
+
+    def __load_data(self):
+        self.p1_list = []
+        for t_step in self.t_list:
+            print("データロード", self.exp_index, f"t={t_step}")
+            p1 = get_probability(self.simulation_data_names, t_step)  # 全体の確率分布
+            self.p1_list.append(p1)
+        print_finish("データの事前ロード")
+
+    def analyze_probability(self):
+        for i, t_step in enumerate(self.t_list):
+            print(self.exp_index, f"t={t_step}")
+            if ConfigSimulationSetting.MaxTimeStep == 2000:
+                p1 = get_probability(self.simulation_data_names, t_step)  # 全体の確率分布
+            else:
+                p1 = self.p1_list[i]
+            p_in_circle, p_out_circle, p_circle = get_prob(prob=p1, radius=self.cut_circle_r,
+                                                           circle_inner_r=self.circle_inner_r,
+                                                           circle_outer_r=self.circle_outer_r)
+            self.p_in_circle_list.append(p_in_circle)
+            self.p_out_circle_list.append(p_out_circle)
+            self.p_circle_list.append(p_circle)
+
+    def save_prob_csv_file(self):
+        with open(f"{self.save_prob_csv_path}/{self.file_name}.csv", mode='w') as f:
+            f.write(
+                f"t,in_circle_{self.exp_index},out_circle_{self.exp_index},circle_{self.exp_index}\n")
+            for i in range(len(self.p_in_circle_list)):
+                s = f"{self.t_list[i]},{self.p_in_circle_list[i]},{self.p_out_circle_list[i]},{self.p_circle_list[i]}\n"
+                f.write(s)
+
+    def analyze_var(self):
+        for i, t_step in enumerate(self.t_list):
+            print(self.exp_index, f"t={t_step}")
+            if ConfigSimulationSetting.MaxTimeStep == 2000:
+                p1 = get_probability(self.simulation_data_names, t_step)  # 全体の確率分布
+            else:
+                p1 = self.p1_list[i]
+            var_p_inner = get_var(prob=p1, radius=self.cut_circle_r)
+            self.var_p_list.append(var_p_inner)
+
+    def save_var_csv_file(self):
+        with open(f"{self.save_var_csv_path}/{self.file_name}.csv", mode='w') as f:
+            f.write(
+                f"t,var_{self.exp_index}\n")
+            for i in range(len(self.var_p_list)):
+                s = f"{self.t_list[i]},{self.var_p_list[i]}\n"
+                f.write(s)
+
+    def analyze_width(self):
+        for i, t_step in enumerate(self.t_list):
+            print(self.exp_index, f"t={t_step}")
+            if ConfigSimulationSetting.MaxTimeStep == 2000:
+                p1 = get_probability(self.simulation_data_names, t_step)  # 全体の確率分布
+            else:
+                p1 = self.p1_list[i]
+
+            # X_width_center, Y_width_center = get_width_center(prob=p1, radius=self.cut_circle_r)
+            # self.X_width_center_list.append(X_width_center)
+            # self.Y_width_center_list.append(Y_width_center)
+            x_left_list = np.zeros([p1.shape[1]], dtype=np.int64)
+            x_right_list = np.zeros([p1.shape[1]], dtype=np.int64)
+            x_width_list = np.zeros([p1.shape[1]], dtype=np.int64)
+            x_width_list, x_left_list, x_right_list = get_width_outer(p1, x_width_list, x_left_list, x_right_list)
+            self.std_prob_width_list.append(np.std(x_width_list))
+            self.std_prob_x_left_list.append(np.std(x_left_list))
+            self.std_prob_x_right_list.append(np.std(x_right_list))
+
+    def save_width_csv_file(self):
+        with open(f"{self.save_width_csv_path}/{self.file_name}.csv", mode='w') as f:
+            f.write(
+                f"t,std_prob_width_{self.exp_index},std_prob_x_left_{self.exp_index},std_prob_x_right_{self.exp_index}\n")
+            for i in range(len(self.std_prob_width_list)):  # 代表して一つの長さを使う
+                s = f"{self.t_list[i]},{self.std_prob_width_list[i]},{self.std_prob_x_left_list[i]},{self.std_prob_x_right_list[i]}\n"
+                f.write(s)
+
+
+@njit('Tuple((f8,f8,f8))(f8[:,:],i8,i8,i8)', cache=True)
+def get_prob(prob, radius, circle_inner_r, circle_outer_r):
+    p_out_circle = 0  # 円形外の確率の和
+    p_in_circle = 0  # 円形内の確率の和
+    p_circle = 0  # 円周付近の確率の和
+
+    len_x = prob.shape[1]
+    len_y = prob.shape[0]
+    T = len_x // 2
+    for x in range(len_x):
+        for y in range(len_y):
+            if (x - T) ** 2 + (y - T) ** 2 < radius ** 2:
+                # 円形内
+                p_in_circle += prob[x, y]
+            else:
+                # 円形外
+                p_out_circle += prob[x, y]
+            if circle_inner_r ** 2 < (x - T) ** 2 + (y - T) ** 2 < circle_outer_r ** 2:
+                # 円周付近の領域内
+                p_circle += prob[x, y]
+
+    # print("in circle", kl_div_in_circle)  # デバッグ用
+    return p_in_circle, p_out_circle, p_circle
+
+
+@njit('f8(f8[:,:],i8)', cache=True)
+def get_var(prob, radius):
+    """
+    prob：確率分布
+    radius：中心付近の確率分布を定義するための半径r
+    circle_inner_r：円周の確率分布を定義するための内半径
+    circle_outer_r：円周の確率分布を定義するための外半径
+    """
+
+    in_circle_p = []  # 中心付近の確率分布のリスト
+
+    len_x = prob.shape[1]
+    len_y = prob.shape[0]
+    T = len_x // 2
+    for x in range(len_x):
+        for y in range(len_y):
+            if (x - T) ** 2 + (y - T) ** 2 < radius ** 2:
+                # 円形内の確率prob[x,y]について分散を求める。
+                in_circle_p.append(prob[x, y])
+
+    in_circle_p = np.array(in_circle_p)
+    # var_inner = np.var(in_circle_p)  # 分散を求める
+    std_inner_p = np.std(in_circle_p)
+    return std_inner_p
+
+
+# @njit('Tuple((i8[:],i8[:],i8[:]))(f8[:,:],i8[:],i8[:],i8[:])', cache=True)
+def get_width_outer(prob, x_width_list, x_left_list, x_right_list):
+    """x、y軸それぞれにおいて、確率分布の最大位置と最小位置の差を求める。"""
+    len_x = prob.shape[0]
+    len_y = prob.shape[1]
+    threshold = 0.005  # 閾値
+    # x_max = 0
+    # x_min = 0
+    # y_max = 0
+    # y_min = 0
+    #
+    # # 初期値として見つかった値を一つ代入しておく
+    # for x in range(len_x):
+    #     for y in range(len_y):
+    #         if prob[x, y] > threshold:
+    #             x_max = x
+    #             x_min = x
+    #             y_max = y
+    #             y_min = y
+    #             break
+    #     else:
+    #         continue
+    #     break
+
+    for y in range(len_y):
+        flag_left = True
+        # 初期値を設定する
+        x_left = 0
+        x_right = 0
+
+        for x in range(len_x):
+            if prob[x, y] > threshold:
+                if flag_left:
+                    # 初めての条件判定なので、最も左の値になる
+                    x_left = x
+                    flag_left = False
+                x_right = x
+
+        x_left_list[y] = x_left
+        x_right_list[y] = x_right
+        x_width_list[y] = x_right - x_left
+
+    return x_width_list, x_left_list, x_right_list
+
+
+@njit('Tuple((i8,i8))(f8[:,:],i8)', cache=True)
+def get_width_center(prob, radius):
+    """
+    半径radiusの円領域の中で、x、y軸それぞれにおいて、確率分布の最大位置と最小位置の差を求める。
+    """
+    len_x = prob.shape[1]
+    len_y = prob.shape[0]
+    threshold = 0.00001  # 閾値
+    # 0,0は初期値として妥当。
+    x_max = 0
+    x_min = 0
+    y_max = 0
+    y_min = 0
+
+    T = len_x // 2
+    for x in range(len_x):
+        for y in range(len_y):
+            if (x - T) ** 2 + (y - T) ** 2 < radius ** 2:
+                # 円形内
+                # 閾値よりも大きい値があれば、更新する。
+                if prob[x, y] > threshold:
+                    if x > x_max:
+                        x_max = x
+                    if x < x_min:
+                        x_min = x
+                    if y > y_max:
+                        y_max = y
+                    if y < y_min:
+                        y_min = y
+
+    return x_max - x_min, y_max - y_min
---
+++ data_analysis/visualization/make_gif.py
@@ -0,0 +1,87 @@
+import glob
+import os
+from PIL import Image
+
+from config.config_visualization import plot_save_path
+from helper import print_finish, print_green_text
+
+
+def output_gif_image(exp_name, plot_type, plot_exp_indexes, plot_t_step, duration=50):
+    """
+    plot_t_stepが指定された場合、全てのplot_exp_indexesのtステップのみプロットしgifにまとめる
+    """
+    if plot_exp_indexes is None:
+        plot_exp_indexes = [0]
+    plotter = Make_gif()
+    plotter.set_up_conditions(exp_name=exp_name, plot_type=plot_type, plot_exp_indexes=plot_exp_indexes,
+                              plot_t_step=plot_t_step, duration=duration)
+    plotter.make_gif()
+
+
+class Make_gif:
+    def __init__(self):
+        self.exp_name = None
+        self.plot_type = None
+        self.plot_exp_indexes = None
+        self.plot_t_step = None
+        self.plot_t_step_zfill = None
+        self.duration = None
+        self.path = None
+        self.save_path = None
+        self.plot_image_path = None
+        self.plot_exp_path = None
+
+    def set_up_conditions(self, exp_name, plot_type, plot_exp_indexes, plot_t_step, duration):
+        self.exp_name = exp_name
+        self.plot_type = plot_type
+        self.plot_exp_indexes = plot_exp_indexes
+        self.plot_t_step = plot_t_step
+        self.plot_t_step_zfill = str(plot_t_step).zfill(4)
+        self.duration = duration
+
+        # 保存先に関する共通事項の設定
+        self.path = f'{gif_save_path(exp_name=self.exp_name, plot_type=plot_type)}'
+        self.plot_exp_path = f"{plot_save_path(exp_name=self.exp_name, plot_type=plot_type)}/*"
+
+    def check_gif_finished(self, plot_exp_index):
+        finished = False
+        # ファイルが存在するかをチェックする
+        if os.path.isfile(self.save_path):
+            finished = True
+            print_green_text(f"exp_index={plot_exp_index}：既に完了")
+        return finished
+
+    def make_gif(self):
+        if self.plot_t_step is None:
+            for plot_exp_index in self.plot_exp_indexes:
+                self.save_path = self.path + f"{str(plot_exp_index).zfill(2)}.gif"
+                if self.check_gif_finished(plot_exp_index):
+                    continue
+                print(f"START：exp_index={plot_exp_index}")
+                # 全ファイル名を取得
+                plot_image_path = f"{plot_save_path(exp_name=self.exp_name, plot_type=self.plot_type, index=plot_exp_index)}/*"
+                plot_image_names = glob.glob(plot_image_path)
+
+                self.__core_make_gif(plot_image_names)
+        else:
+            # resultのplotの中にあるexpフォルダ名を取得
+            folder_names = glob.glob(self.plot_exp_path)
+            plot_image_names = [folder_name + f"/{self.plot_t_step_zfill}.png" for folder_name in folder_names]
+            self.save_path = self.path + f"t_{str(self.plot_t_step).zfill(4)}.gif"
+            self.__core_make_gif(plot_image_names)
+
+    def __core_make_gif(self, plot_image_names):
+        plot_image_names.sort()
+        frames = []
+        for plot_image in plot_image_names:
+            new_frame = Image.open(plot_image)
+            frames.append(new_frame)
+        frames[0].save(
+            self.save_path,
+            format='GIF',
+            append_images=frames[1:],
+            save_all=True,
+            duration=self.duration,
+            loop=0,
+            optimize=False)
+        print_finish(self.plot_type)
