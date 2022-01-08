d2c47fd71b5f431c77be3cc995d34db3fc1f399c
中山昌勲 <>
2022-01-09 01:18:25 +0900

    データクラスへ対応させ、コードを変えた

--- simulation_result_analyzer_and_plotter.py
+++ simulation_result_analyzer_and_plotter.py
@@ -1,72 +1,122 @@
-from analyze.visualization.analyzer import Analyzer
-from analyze.visualization.plot_analyze import AnalyzePlotter, OptimizePlotter
+from config.config_data_analyze import AnalyzeNameSetting, DefaultAnalyzeSetting, DefaultAnalyzeOptimizeSetting, \
+    DefaultAnalyzePlotSetting, DefaultOptimizePlotSetting, OptimizeNameSetting
+from data_analysis.data_analyze.analyzer import Analyzer, AnalyzeOptimizer
+from data_analysis.data_analyze.plot_analyze import AnalyzePlotter, OptimizePlotter
 from qw import *
-
-DefaultAnalyzeSetting = {
-    "parallel": False,  # 処理を並列化するかどうか。現状Falseのほうが高速
-    "KL_div": True,  # KLダイバージェンスを求めるかどうか
-    "L1_norm": True,  # L1ノルムを求めるかどうか（誤差の絶対値の和）
-    "L2_norm": True,  # L2ノルムを求めるかどうか（二乗誤差の和）
-    "correlation_coefficient": True  # 相関係数を求めるかどうか
-}
+import dataclasses
 
 
 class SimulationResultAnalyzer:
-    def __init__(self, qw1, qw2, analyze_indexes, options=None):
-        self.__qw1 = qw1
-        self.__qw2 = qw2
-        self.__analyze_indexes = analyze_indexes
-        if options is None:
-            options = {}
-        self.__options = {**DefaultAnalyzeSetting, **options}
-        # パラメータをチェック
-        self.__check_params()
+    def __init__(self, qw1, qw2, _analyze_indexes):
+        self.__exp1_name = qw1.conditions[0].exp_name  # 代表して0番目を使用
+        self.__exp2_name = qw2.conditions[0].exp_name  # 代表して0番目を使用
+        self.__exp1_index = 0  # 代表して0を使用
+        self.__analyze_indexes = _analyze_indexes
 
-    def __check_params(self):
-        if self.__options is None:
-            self.__options = {}
-
-    def analyze(self):
-        analyzer_ = Analyzer(self.__qw1, self.__qw2, self.__analyze_indexes, self.__options, mode="analyze")
+    def analyze(self, options=None):
+        _setting = DefaultAnalyzeSetting()
+        if options is not None:
+            _setting = dataclasses.replace(_setting, **options)
+        analyzer_ = Analyzer(self.__exp1_name, self.__exp2_name, self.__exp1_index, self.__analyze_indexes,
+                             setting=_setting)
         analyzer_.start_processing()
 
-    def plot_x_axis_is_index(self, plot_t_list):
-        plotter = AnalyzePlotter(self.__qw1, self.__qw2, self.__analyze_indexes, plot_t_list, x_axis="index")
+    def plot_x_axis_is_index(self, _plot_t_list, options=None):
+        # デフォルト設定
+        path_to_file = AnalyzeNameSetting(exp1_name=self.__exp1_name, exp2_name=self.__exp2_name,
+                                          exp1_index=self.__exp1_index).path_to_file
+        _setting = DefaultAnalyzePlotSetting(x_label="t_{erase}", y_label="", legend_label="t",
+                                             plot_indexes=_plot_t_list, path_to_file=path_to_file,
+                                             file_name="", x_axis="index")
+        # オプション設定があれば適用
+        if options is not None:
+            _setting = dataclasses.replace(_setting, **options)
+
+        plotter = AnalyzePlotter(self.__exp1_name, self.__exp2_name, self.__exp1_index, self.__analyze_indexes,
+                                 _plot_t_list, _setting)
         plotter.plot()
 
-    def plot_x_axis_is_t(self, plot_indexes, start_t=0):
-        plotter = AnalyzePlotter(self.__qw1, self.__qw2, self.__analyze_indexes, plot_indexes, x_axis="t",
-                                 start_t=start_t)
+    def plot_x_axis_is_t(self, _plot_indexes, start_t=0, options=None):
+        """
+        x軸に時間ステップtをとってプロットする
+        Args:
+            _plot_indexes:
+            start_t: x軸の原点
+            options:オプション設定
+        """
+        # デフォルト設定
+        path_to_file = AnalyzeNameSetting(exp1_name=self.__exp1_name, exp2_name=self.__exp2_name,
+                                          exp1_index=self.__exp1_index).path_to_file
+        _setting = DefaultAnalyzePlotSetting(x_label="t", y_label="", legend_label="t_{erase}",
+                                             plot_indexes=_plot_indexes, start_t=start_t, path_to_file=path_to_file,
+                                             file_name="", x_axis="t")
+        # オプション設定があれば適用
+        if options is not None:
+            _setting = dataclasses.replace(_setting, **options)
+
+        plotter = AnalyzePlotter(self.__exp1_name, self.__exp2_name, self.__exp1_index, self.__analyze_indexes,
+                                 _plot_indexes, _setting)
         plotter.plot()
 
-    def analyze_for_optimization_t(self, analyze_t):
+    def analyze_for_optimization_t(self, analyze_t, options=None):
         """
         self.__qw1とself.__qw2の確率分布の類似性について、self.__analyze_indexesに指定された実験全てにおいて、analyze_tステップ目の
         qw2の確率分布と最も確率分布の近いqw1の時間ステップtを求める。
         Args:
             analyze_t: 調べる際の時間ステップ
+            options:オプション設定
 
         Returns:
 
         """
-        analyzer_ = Analyzer(self.__qw1, self.__qw2, self.__analyze_indexes, self.__options, mode="optimize")
-        analyzer_.optimize_t(analyze_t=analyze_t)
+        _setting = DefaultAnalyzeOptimizeSetting(analyze_t=analyze_t)
+        if options is not None:
+            _setting = dataclasses.replace(_setting, **options)
+        analyzer_ = AnalyzeOptimizer(self.__exp1_name, self.__exp2_name, self.__exp1_index, self.__analyze_indexes,
+                                     setting=_setting)
+        analyzer_.optimize_t_all()
 
-    def plot_optimize_t(self, analyze_t):
+    def print_optimize_t(self, analyze_t):
+        _setting = DefaultOptimizePlotSetting(x_label="k", y_label="", title="", x_axis_data_list=[],
+                                              y_axis_data_list=[], path_to_file="", file_name="", analyze_t=analyze_t)
+        plotter = OptimizePlotter(self.__exp1_name, self.__exp2_name, self.__exp1_index, self.__analyze_indexes,
+                                  setting=_setting)
+        plotter.print_all_optimize_result()
+
+    def plot_optimize_x_axis_is_index(self, analyze_t):
         plotter = OptimizePlotter(self.__qw1, self.__qw2, self.__analyze_indexes, analyze_t)
-        plotter.print_optimized_result()
+        plotter.plot_optimize_result_x_axis_is_index()
+
+    def plot_optimize_x_axis_is_rank(self, analyze_t, options=None):
+        name_setting = OptimizeNameSetting(exp1_name=self.__exp1_name, exp1_index=self.__exp1_index,
+                                           exp2_name=self.__exp2_name, analyze_t=analyze_t)
+
+        _setting = DefaultOptimizePlotSetting(x_label="rank", y_label="t", title="", x_axis_data_list=[],
+                                              y_axis_data_list=[], path_to_file=name_setting.path_to_file,
+                                              file_name=name_setting.file_name + "x_axis_is_index", analyze_t=analyze_t)
+        # オプション設定があれば適用
+        if options is not None:
+            _setting = dataclasses.replace(_setting, **options)
+
+        plotter = OptimizePlotter(self.__exp1_name, self.__exp2_name, self.__exp1_index, self.__analyze_indexes,
+                                  setting=_setting)
+        plotter.plot_optimize_result_x_axis_is_rank()
 
 
 if __name__ == '__main__':
-    indexes = [50]
+    analyze_indexes = [10, 20, 30, 40, 50]
     analyzer = SimulationResultAnalyzer(qw1=GroverWalk2D(),
-                                        qw2=EraseElectricGroverWalk2DAlongX(erase_t_list=indexes),
-                                        analyze_indexes=indexes,
-                                        options={"parallel": False})
+                                        qw2=EraseElectricGroverWalk2DAlongX(erase_t_list=analyze_indexes),
+                                        _analyze_indexes=analyze_indexes)
+    # analyze処理を実行
     # analyzer.analyze()
+    # その結果をプロット
+    # plot_indexes = analyze_indexes
+    # analyzer.plot_x_axis_is_index(_plot_t_list=plot_indexes, )
+
+    # 最適な時間ステップを求める
     # analyzer.analyze_for_optimization_t(analyze_t=200)
-    analyzer.plot_optimize_t(analyze_t=200)
-    # t_list = [200, 300, 400, 500, 600]
-    # analyzer.plot_x_axis_is_index(plot_t_list=t_list)
-    # plot_indexes = [0]
-    # analyzer.plot_x_axis_is_t(plot_indexes=plot_indexes, start_t=0)
+    # その結果をプロット
+    analyzer.print_optimize_t(analyze_t=200)
+    # analyzer.print_optimize_t(analyze_t=600)
+    # analyzer.plot_optimize_x_axis_is_index(analyze_t=600)
