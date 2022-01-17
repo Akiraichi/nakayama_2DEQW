29f46aa444577892bcc5fd4e089bf7c0a660c57c
中山昌勲 <>
2022-01-17 17:11:14 +0900

    コメントを追加

--- simulation_plot.py
+++ simulation_plot.py
@@ -9,7 +9,11 @@ from simulation.conditions_factories.conditions_erase_t_factory import Condition
 class SimulationResultPlotter:
     def __init__(self, conditions, save_path_indexes):
         """
-
+        plot_t_listで指定されたtのプロットを行う。
+        例：plot_t_list=[200]の場合
+        0200.jbをプロットする。0200.jbは200ステップ目が完了した時点（つまり、t=200）のシステム全体の確率振幅ベクトルを保存したファイル
+        例：ψ_{t=200}をプロットしたい場合
+        plot_t_list=[200]とする。
         Args:
             conditions: 実験条件をリストで指定したもの
             save_path_indexes: 保存する際のフォルダ名に使うindexのリスト
@@ -46,12 +50,13 @@ class SimulationResultPlotter:
 
 
 if __name__ == '__main__':
-    indexes = [90]
+    indexes = [0]
     plotter = SimulationResultPlotter(
-        conditions=ConditionsEraseTFactory.EraseT_005_EraseElectricGroverWalk2DAlongX(erase_t_list=[90]),
+        conditions=ConditionsSingleFactory.single_009_ElectricGroverWalk2DAlongXY(),
         save_path_indexes=indexes)
-    # plotter.plot_surface()
-    # plotter.plot_heatmap()
+    plotter.plot_surface()
+    # plotter.plot_heatmap(options={"plot_t_list": [118]})
+    plotter.plot_heatmap()
 
-    plotter.plot_3d_image(options={"plot_t_list": list(range(20, 205, 5))})
+    # plotter.plot_3d_image(options={"plot_t_list": list(range(20, 205, 5))})
     # plotter.plot_3d_image()
