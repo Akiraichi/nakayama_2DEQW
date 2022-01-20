3d558c25434b658d46fe3a0014872f14580657b2
中山昌勲 <>
2022-01-20 19:39:25 +0900

    dpiの設定を追加した

--- simulation_plot.py
+++ simulation_plot.py
@@ -25,14 +25,14 @@ class SimulationResultPlotter:
 
     def plot_surface(self, options=None):
         plot_type = "surface"
-        _setting = DefaultPlotSetting(plot_type, self.__conditions, self.__save_path_indexes)
+        _setting = DefaultPlotSetting(plot_type, self.__conditions, 800, self.__save_path_indexes)
         if options is not None:
             _setting = dataclasses.replace(_setting, **options)
         plot_image(_setting=_setting)
 
     def plot_heatmap(self, options=None):
         plot_type = "heatmap"
-        _setting = DefaultPlotSetting(plot_type, self.__conditions, self.__save_path_indexes)
+        _setting = DefaultPlotSetting(plot_type, self.__conditions, 400, self.__save_path_indexes)
         if options is not None:
             _setting = dataclasses.replace(_setting, **options)
         plot_image(_setting=_setting)
@@ -53,32 +53,54 @@ class SimulationResultPlotter:
 
     def plot_surface_group_by(self, t_of_plot_list):
         plot_type = "surface"
-        _setting = DefaultPlotSetting(plot_type, self.__conditions, self.__save_path_indexes)
+        _setting = DefaultPlotSetting(plot_type, self.__conditions, 800, self.__save_path_indexes)
         options = {"plot_t_list": t_of_plot_list}
         _setting = dataclasses.replace(_setting, **options)
         plot_image_group(_setting=_setting)
 
     def plot_heatmap_group_by(self, t_of_plot_list):
         plot_type = "heatmap"
-        _setting = DefaultPlotSetting(plot_type, self.__conditions, self.__save_path_indexes)
+        _setting = DefaultPlotSetting(plot_type, self.__conditions, 400, self.__save_path_indexes)
         options = {"plot_t_list": t_of_plot_list}
         _setting = dataclasses.replace(_setting, **options)
         plot_image_group(_setting=_setting)
 
 
 if __name__ == '__main__':
-    indexes = [0]
-    # indexes = [16, 31, 46, 61, 76, 91, 106, 121, 136]
+    # indexes = [0]
+    indexes = [16, 31, 46, 61, 76, 91, 106, 121, 136]
+    plotter = SimulationResultPlotter(
+        conditions=ConditionsEraseTFactory.EraseT_005_EraseElectricGroverWalk2DAlongX(erase_t_list=indexes),
+        save_path_indexes=indexes)
+    # plotter = SimulationResultPlotter(
+    #     conditions=ConditionsSingleFactory.single_007_GroverWalk2D(),
+    #     save_path_indexes=indexes)
+    # plotter.plot_surface()
+    # plotter.plot_heatmap(options={"plot_t_list": [118]})
+    plotter.plot_heatmap()
+
+    # plotter.plot_heatmap_group_by(t_of_plot_list=[200] * len(indexes))
+    # plotter.plot_surface_group_by(t_of_plot_list=[200] * len(indexes))
+    # plotter.plot_3d_images(plot_t_list=[15, 30, 45, 60, 75, 90, 105, 120, 135], z_axis="t")
+    # plotter.plot_3d_images(plot_t_list=[15, 30, 45], z_axis="t")
+    # plotter.plot_3d_images(plot_t_list=[200], z_axis="i")
+
+    # indexes = [121]
+    # indexes = list(range(16, 46, 15))
     # plotter = SimulationResultPlotter(
     #     conditions=ConditionsEraseTFactory.EraseT_005_EraseElectricGroverWalk2DAlongX(erase_t_list=indexes),
     #     save_path_indexes=indexes)
-    plotter = SimulationResultPlotter(
-        conditions=ConditionsSingleFactory.single_007_GroverWalk2D(),
-        save_path_indexes=indexes)
-    # plotter.plot_surface()
-    # plotter.plot_heatmap(options={"plot_t_list": [118]})
-    # plotter.plot_heatmap()
+    # plotter.plot_3d_images(plot_t_list=list(range(15, 210, 15)), z_axis="t")
+    # plotter.plot_3d_images(plot_t_list=list(range(60,660,60)), z_axis="t")
+    # plotter.plot_3d_images(plot_t_list=list(range(30,630,30)), z_axis="t")
+    # plotter.plot_3d_images(plot_t_list=[100, 200], z_axis="t")
 
-    # plotter.plot_heatmap_group_by(t_of_plot_list=[200] * len(indexes))
-    plotter.plot_3d_images(plot_t_list=[15, 30, 45, 60, 75, 90, 105, 120, 135], z_axis="t")
-    # plotter.plot_3d_images(plot_t_list=[200], z_axis="i")
+    # indexes = list(range(16, 256, 15))
+    # indexes = [121]
+    # plotter = SimulationResultPlotter(
+    #     conditions=ConditionsEraseTFactory.EraseT_006_EraseElectricGroverWalk2DAlongXY(erase_t_list=indexes),
+    #     save_path_indexes=indexes)
+    # plotter.plot_3d_images(plot_t_list=[120, 240, 360, 480, 600], z_axis="t")
+    # plotter.plot_3d_images(plot_t_list=list(range(60, 660, 60)), z_axis="t")
+    # plotter.plot_3d_images(plot_t_list=list(range(30, 630, 30)), z_axis="t")
+    # plotter.plot_3d_images(plot_t_list=[600], z_axis="i")
